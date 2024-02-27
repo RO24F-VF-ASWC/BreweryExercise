ed0dc143111276a1535ba2f193f60ebdf3e2df3f
Peter Levinsky <pele@zealand.dk>
2024-02-27 09:40:09 +0100

    Add project files.

---
+++ BreweryApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BreweryApp;
+using System.Diagnostics;
+
+TraceSource TRACE = new TraceSource("Brewery");
+TRACE.Listeners.Add(
+    new TextWriterTraceListener(
+        new StreamWriter(@"M:\pub\Brewery.txt")));
+TRACE.Listeners.Add(new ConsoleTraceListener());
+
+
+TRACE.Switch = new SourceSwitch("Brewery", "all");
+
+BreweryWorker worker = new BreweryWorker(TRACE);
+worker.Start();
+
+Console.WriteLine("Simulation finished - look in logfile");
+Console.ReadLine();
